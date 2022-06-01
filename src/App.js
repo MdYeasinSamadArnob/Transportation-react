dbbffa51d3574034843a373cf5656c948bcac924
SadikHasanRafi <sadikkhasan@gmail.com>
2022-06-02 01:27:02 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './style.css';
 
 export default function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 class="text-3xl font-bold underline">diu transport!</h1>;
 }
